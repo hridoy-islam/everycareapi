c6f0edce3644c2d2472857918cf9565834bfab1d
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-09-08 20:01:11 +0600

    update user model and interface for career

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -168,13 +168,14 @@ export interface EnglishQualification {
 }
 
 export interface Referee {
-  name?: string;
-  organisation?: string;
-  address?: string;
-  relationship?: string;
-  otherRelationship?: string;
-  email?: string;
-  phone?: string;
+  name: string;
+  position: string;
+  relationship: string;
+  organisation: string;
+  address: string;
+  tel: string;
+  fax?: string;
+  email: string;
 }
 
 
@@ -200,7 +201,9 @@ export interface TUser {
   isCompleted: boolean;
   userAgentInfo: UserAgentInfo[];
 
+  // -----------------------
   // Personal Info
+  // -----------------------
   title?: string;
   firstName?: string;
   lastName?: string;
@@ -213,46 +216,84 @@ export interface TUser {
   shareCode?: string;
   nationalInsuranceNumber?: string;
 
+  // -----------------------
   // Postal Address
+  // -----------------------
   postalAddressLine1?: string;
   postalAddressLine2?: string;
   postalCity?: string;
   postalPostCode?: string;
   postalCountry?: string;
 
-  // Previous Address
-  prevPostalAddressLine1?: string;
-  prevPostalAddressLine2?: string;
-  prevPostalCity?: string;
-  prevPostalPostCode?: string;
-  prevPostalCountry?: string;
-
+  // -----------------------
   // Emergency Contact
+  // -----------------------
   emergencyContactNumber?: string;
   emergencyEmail?: string;
   emergencyFullName?: string;
   emergencyRelationship?: string;
   emergencyAddress?: string;
 
-  // Application & Career
-  source?: string;
-  referralEmployee?: string;
-  availableFromDate?: Date;
-  isOver18?: boolean;
-  isSubjectToImmigrationControl?: boolean;
-  canWorkInUK?: boolean;
-  availability?: {
-    [day: string]: boolean;
-  };
-  isStudent?: boolean;
-  isUnderStatePensionAge?: boolean;
+  // -----------------------
+  // Bank Details
+  // -----------------------
+  houseNumberOrName?: string;
+  postCode?: string;
+  jobRole?: string;
+  otherJobRole?: string;
+  accountNumber?: string;
+  sortCode?: string;
+  bankName?: string;
+  branchName?: string;
+  buildingSocietyRollNo?: string;
 
+  // -----------------------
+  // Application & Career
+  // -----------------------
+    availableFromDate: Date;
+  source: string;
+  referralEmployee?: string;
+
+  availability: {
+    monday?: boolean;
+    tuesday?: boolean;
+    wednesday?: boolean;
+    thursday?: boolean;
+    friday?: boolean;
+    saturday?: boolean;
+    sunday?: boolean;
+  };
+
+  isStudent: boolean;
+  isUnderStatePensionAge: boolean;
+  isOver18: boolean;
+  isSubjectToImmigrationControl: boolean;
+  canWorkInUK: boolean;
+
+  competentInOtherLanguage: boolean;
+  otherLanguages?: string[];
+
+  drivingLicense: boolean;
+  licenseNumber?: string;
+
+  carOwner: boolean;
+  travelAreas: string;
+
+  solelyForEverycare: boolean;
+  otherEmployers?: string;
+
+  professionalBody: boolean;
+  professionalBodyDetails?: string;
+
+
+  // -----------------------
   // Employment
+  // -----------------------
   isEmployed: string;
   currentEmployment?: {
     employer?: string;
     jobTitle?: string;
-    startDate?: string;
+    startDate?: string; // using string because of API serialization
     employmentType?: string;
     responsibilities?: string;
     supervisor?: string;
@@ -260,18 +301,22 @@ export interface TUser {
   };
   hasPreviousEmployment?: string;
   previousEmployments?: {
-    employer: string;
-    jobTitle: string;
-    startDate: string;
-    endDate: string;
-    reasonForLeaving: string;
-    responsibilities: string;
-    contactPermission: string;
+    employer?: string;
+    jobTitle?: string;
+    startDate?: string;
+    endDate?: string;
+    reasonForLeaving?: string;
+    responsibilities?: string;
+    hasEmploymentGaps?: string;
+    employmentGapsExplanation?: string;
+    contactPermission?: string;
   }[];
   hasEmploymentGaps?: string;
   employmentGapsExplanation?: string;
 
+  // -----------------------
   // Education
+  // -----------------------
   educationData: {
     institution: string;
     qualification: string;
@@ -280,40 +325,196 @@ export interface TUser {
     certificate?: string;
   }[];
 
-  // English Qualification
-  englishQualification?: EnglishQualification;
+  // -----------------------
+  // Training
+  // -----------------------
+  trainingData?: {
+    name: string;
+    address: string;
+    from: Date | null;
+    to: Date | null;
+    qualification?: string;
+  }[];
 
-  // Disability
-  hasDisability?: boolean;
-  disabilityDetails?: string;
-  needsReasonableAdjustment?: boolean;
-  reasonableAdjustmentDetails?: string;
+  // -----------------------
+  // Ethnicity
+  // -----------------------
+  ethnicityGroup?: string;
+  ethnicityValue?: string;
+  ethnicityOther?: string;
 
+  // -----------------------
+  // Life Skills & Experience
+  // -----------------------
+  lifeSkillsAndInterests?: string;
+  relevantExperience?: string;
+
+  // -----------------------
   // References
-  referee1?: Referee;
-  referee2?: Referee;
+  // -----------------------
+  professionalReferee1?: Referee;
+  professionalReferee2?: Referee;
+  personalReferee?: Referee;
 
+  // -----------------------
   // Documents
-  proofOfAddress: string[];
-  passport: string[];
-  immigrationDocument: string[];
-  workExperience: string[];
-  bankStatement: string[];
-  personalStatement: string[];
+  // -----------------------
+  cvResume?: string;
+  idDocuments?: string[];
+  passportPhotos?: string[];
+  utilityBills?: string[];
+  bankStatement?: string[];
+  proofOfNI?: string[];
+  immigrationDocument?: string[];
+  proofOfAddress?: string[];
+  passport?: string[];
+  workExperience?: string[];
+  personalStatement?: string[];
 
-  // Terms & Declarations
+  // -----------------------
+  // Health & Medical
+  // -----------------------
+
+  sex: string;
+  advisedMedicalWorkRestriction?: boolean;
+  advisedMedicalWorkRestrictionComment?: string;
+  undueFatigue?: boolean;
+  undueFatigueDetails?: string;
+  bronchitis?: boolean;
+  bronchitisDetails?: string;
+  breathlessness?: boolean;
+  breathlessnessDetails?: string;
+  allergies?: boolean;
+  allergiesDetails?: string;
+  pneumonia?: boolean;
+  pneumoniaDetails?: string;
+  hayFever?: boolean;
+  hayFeverDetails?: string;
+  shortnessOfBreath?: boolean;
+  shortnessOfBreathDetails?: string;
+  jundice?: boolean;
+  jundiceDetails?: string;
+  stomachProblems?: boolean;
+  stomachProblemsDetails?: string;
+  stomachUlcer?: boolean;
+  stomachUlcerDetails?: string;
+  hernias?: boolean;
+  herniasDetails?: string;
+  bowelProblem?: boolean;
+  bowelProblemDetails?: string;
+  diabetesMellitus?: boolean;
+  diabetesMellitusDetails?: string;
+  nervousDisorder?: boolean;
+  nervousDisorderDetails?: string;
+  dizziness?: boolean;
+  dizzinessDetails?: string;
+  earProblems?: boolean;
+  earProblemsDetails?: string;
+  hearingDefect?: boolean;
+  hearingDefectDetails?: string;
+  epilepsy?: boolean;
+  epilepsyDetails?: string;
+  eyeProblems?: boolean;
+  eyeProblemsDetails?: string;
+  ppeAllergy?: boolean;
+  ppeAllergyDetails?: string;
+  rheumaticFever?: boolean;
+  rheumaticFeverDetails?: string;
+  highBloodPressure?: boolean;
+  highBloodPressureDetails?: string;
+  lowBloodPressure?: boolean;
+  lowBloodPressureDetails?: string;
+  palpitations?: boolean;
+  palpitationsDetails?: string;
+  heartAttack?: boolean;
+  heartAttackDetails?: string;
+  angina?: boolean;
+  anginaDetails?: string;
+  asthma?: boolean;
+  asthmaDetails?: string;
+  chronicLungProblems?: boolean;
+  chronicLungProblemsDetails?: string;
+  stroke?: boolean;
+  strokeDetails?: string;
+  heartMurmur?: boolean;
+  heartMurmurDetails?: string;
+  backProblems?: boolean;
+  backProblemsDetails?: string;
+  jointProblems?: boolean;
+  jointProblemsDetails?: string;
+  swollenLegs?: boolean;
+  swollenLegsDetails?: string;
+  varicoseVeins?: boolean;
+  varicoseVeinsDetails?: string;
+  rheumatism?: boolean;
+  rheumatismDetails?: string;
+  migraine?: boolean;
+  migraineDetails?: string;
+  drugReaction?: boolean;
+  drugReactionDetails?: string;
+  visionCorrection?: boolean;
+  visionCorrectionDetails?: string;
+  skinConditions?: boolean;
+  skinConditionsDetails?: string;
+  alcoholHealthProblems?: boolean;
+  alcoholHealthProblemsDetails?: string;
+  seriousIllnessDetails?: string;
+  recentIllHealth?: boolean;
+  recentIllHealthComment?: string;
+  attendingClinic?: boolean;
+  attendingClinicComment?: string;
+  hadChickenPox?: boolean;
+  hadChickenPoxComment?: string;
+  otherCommunicableDisease?: boolean;
+  otherCommunicableDiseaseComment?: string;
+
+  // -----------------------
+  // Inoculations / Vaccinations
+  // -----------------------
+  inocDiphtheria?: boolean;
+  inocDiphtheriaComment?: string;
+  inocHepatitisB?: boolean;
+  inocHepatitisBComment?: string;
+  inocTuberculosis?: boolean;
+  inocTuberculosisComment?: string;
+  inocRubella?: boolean;
+  inocRubellaComment?: string;
+  inocVaricella?: boolean;
+  inocVaricellaComment?: string;
+  inocPolio?: boolean;
+  inocPolioComment?: string;
+  inocTetanus?: boolean;
+  inocTetanusComment?: string;
+  testedHIV?: boolean;
+  testedHIVComment?: string;
+  inocOther?: string;
+  inocOtherComment?: string;
+
+  daysSickLastYear?: string;
+
+  // -----------------------
+  // Consent & Declarations
+  // -----------------------
   declarationCorrectUpload?: boolean;
   declarationContactReferee?: boolean;
-  criminalConviction?: boolean;
-  criminalConvictionDetails?: string;
+  authorizeReferees?: boolean;
+  disciplinaryInvestigation?: boolean;
+  disciplinaryInvestigationDetails?: string;
+  abuseInvestigation?: boolean;
+  abuseInvestigationDetails?: string;
+  roaDeclaration?: boolean;
+  roaDeclarationDetails?: string;
   appliedBefore?: boolean;
-  dataProcessingAccepted?: boolean;
   termsAccepted?: boolean;
+  dataProcessingAccepted?: boolean;
+  consentMedicalDeclaration?: boolean;
+  consentDataProtection?: boolean;
+  consentVaccination?: boolean;
+  consentTerminationClause?: boolean;
 
   createdAt?: Date;
   updatedAt?: Date;
 }
-
 export interface UserModel extends Model<TUser> {
   //instance methods for checking if the user exist
   isUserExists(email: string): Promise<TUser>;
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -113,7 +113,6 @@ const userSchema = new Schema<TUser, UserModel>(
     lastName: { type: String },
     otherName: { type: String },
     initial: { type: String },
-    // gender: { type: String },
     dateOfBirth: { type: Date },
     nationality: { type: String },
     countryOfResidence: { type: String },
@@ -121,18 +120,12 @@ const userSchema = new Schema<TUser, UserModel>(
     shareCode: { type: String },
     nationalInsuranceNumber: { type: String },
 
+    // Postal Address
     postalAddressLine1: { type: String },
     postalAddressLine2: { type: String },
     postalCity: { type: String },
     postalPostCode: { type: String },
     postalCountry: { type: String },
-
-    //career previous address
-    prevPostalAddressLine1: { type: String },
-    prevPostalAddressLine2: { type: String },
-    prevPostalCity: { type: String },
-    prevPostalPostCode: { type: String },
-    prevPostalCountry: { type: String },
 
     // Emergency Contact
     emergencyContactNumber: { type: String },
@@ -141,23 +134,43 @@ const userSchema = new Schema<TUser, UserModel>(
     emergencyRelationship: { type: String },
     emergencyAddress: { type: String },
 
-    //career applicants
-
-    source: { type: String },
+    // Career/Application
+  availableFromDate: { type: Date},
+    source: { type: String},
     referralEmployee: { type: String },
-    availableFromDate: { type: Date },
-    isOver18: { type: Boolean },
-    isSubjectToImmigrationControl: { type: Boolean },
-    canWorkInUK: { type: Boolean },
-    // Weekly Availability
+
     availability: {
-      type: Map,
-      of: Boolean,
+      monday: { type: Boolean },
+      tuesday: { type: Boolean },
+      wednesday: { type: Boolean },
+      thursday: { type: Boolean },
+      friday: { type: Boolean },
+      saturday: { type: Boolean },
+      sunday: { type: Boolean },
     },
-    isStudent: { type: Boolean },
-    isUnderStatePensionAge: { type: Boolean },
-    
-   
+
+    isStudent: { type: Boolean},
+    isUnderStatePensionAge: { type: Boolean},
+    isOver18: { type: Boolean},
+    isSubjectToImmigrationControl: { type: Boolean},
+    canWorkInUK: { type: Boolean},
+
+    competentInOtherLanguage: { type: Boolean},
+    otherLanguages: [{ type: String }],
+
+    drivingLicense: { type: Boolean},
+    licenseNumber: { type: String },
+
+    carOwner: { type: Boolean},
+    travelAreas: { type: String},
+
+    solelyForEverycare: { type: Boolean},
+    otherEmployers: { type: String },
+
+    professionalBody: { type: Boolean},
+    professionalBodyDetails: { type: String },
+
+
 
     // Employment
     isEmployed: { type: String },
@@ -171,7 +184,6 @@ const userSchema = new Schema<TUser, UserModel>(
       contactPermission: { type: String },
     },
     hasPreviousEmployment: { type: String },
-
     previousEmployments: [
       {
         employer: { type: String },
@@ -180,6 +192,8 @@ const userSchema = new Schema<TUser, UserModel>(
         endDate: { type: String },
         reasonForLeaving: { type: String },
         responsibilities: { type: String },
+        hasEmploymentGaps: { type: String },
+        employmentGapsExplanation: { type: String },
         contactPermission: { type: String },
       },
     ],
@@ -200,62 +214,227 @@ const userSchema = new Schema<TUser, UserModel>(
       default: [],
     },
 
-    englishQualification: {
-      type: {
-        englishTestType: { type: String },
-        englishTestScore: { type: String },
-        englishTestDate: { type: Date },
-        englishCertificate: { type: String },
-      },
+    // Training
+    trainingData: {
+      type: [
+        {
+          name: { type: String },
+          address: { type: String },
+          from: { type: Date },
+          to: { type: Date },
+          qualification: { type: String },
+        },
+      ],
+      default: [],
     },
 
-    disabilityDetails: { type: String },
-    hasDisability: { type: Boolean },
+    // Ethnicity
+    ethnicityGroup: { type: String },
+    ethnicityValue: { type: String },
+    ethnicityOther: { type: String },
 
-    needsReasonableAdjustment: { type: Boolean },
-    reasonableAdjustmentDetails: { type: String },
+    // Life Skills & Experience
+    lifeSkillsAndInterests: { type: String },
+    relevantExperience: { type: String },
 
-    // References
-    referee1: {
+    // References (Updated to match Referee interface)
+    professionalReferee1: {
       name: { type: String },
+      position: { type: String },
+      relationship: { type: String },
       organisation: { type: String },
       address: { type: String },
-      relationship: { type: String },
-      otherRelationship: { type: String },
+      tel: { type: String },
+      fax: { type: String },
       email: { type: String },
-      phone: { type: String },
     },
-    referee2: {
+    professionalReferee2: {
       name: { type: String },
+      position: { type: String },
+      relationship: { type: String },
       organisation: { type: String },
       address: { type: String },
-      relationship: { type: String },
-      otherRelationship: { type: String },
+      tel: { type: String },
+      fax: { type: String },
       email: { type: String },
-      phone: { type: String },
+    },
+    personalReferee: {
+      name: { type: String },
+      position: { type: String },
+      relationship: { type: String },
+      organisation: { type: String },
+      address: { type: String },
+      tel: { type: String },
+      fax: { type: String },
+      email: { type: String },
     },
 
-    //documents
+    // Documents
+    cvResume: { type: String },
+    idDocuments: { type: [String], default: [] },
+    passportPhotos: { type: [String], default: [] },
+    utilityBills: { type: [String], default: [] },
+    bankStatement: { type: [String], default: [] },
+    proofOfNI: { type: [String], default: [] },
+    immigrationDocument: { type: [String], default: [] },
     proofOfAddress: { type: [String], default: [] },
     passport: { type: [String], default: [] },
-    immigrationDocument: { type: [String], default: [] },
     workExperience: { type: [String], default: [] },
-    bankStatement: { type: [String], default: [] },
     personalStatement: { type: [String], default: [] },
 
-    //terms and conditions
+    // Health & Medical
+    sex:{type: String},
+    advisedMedicalWorkRestriction: { type: Boolean },
+    advisedMedicalWorkRestrictionComment: { type: String },
+    undueFatigue: { type: Boolean },
+    undueFatigueDetails: { type: String },
+    bronchitis: { type: Boolean },
+    bronchitisDetails: { type: String },
+    breathlessness: { type: Boolean },
+    breathlessnessDetails: { type: String },
+    allergies: { type: Boolean },
+    allergiesDetails: { type: String },
+    pneumonia: { type: Boolean },
+    pneumoniaDetails: { type: String },
+    hayFever: { type: Boolean },
+    hayFeverDetails: { type: String },
+    shortnessOfBreath: { type: Boolean },
+    shortnessOfBreathDetails: { type: String },
+    jundice: { type: Boolean },
+    jundiceDetails: { type: String },
+    stomachProblems: { type: Boolean },
+    stomachProblemsDetails: { type: String },
+    stomachUlcer: { type: Boolean },
+    stomachUlcerDetails: { type: String },
+    hernias: { type: Boolean },
+    herniasDetails: { type: String },
+    bowelProblem: { type: Boolean },
+    bowelProblemDetails: { type: String },
+    diabetesMellitus: { type: Boolean },
+    diabetesMellitusDetails: { type: String },
+    nervousDisorder: { type: Boolean },
+    nervousDisorderDetails: { type: String },
+    dizziness: { type: Boolean },
+    dizzinessDetails: { type: String },
+    earProblems: { type: Boolean },
+    earProblemsDetails: { type: String },
+    hearingDefect: { type: Boolean },
+    hearingDefectDetails: { type: String },
+    epilepsy: { type: Boolean },
+    epilepsyDetails: { type: String },
+    eyeProblems: { type: Boolean },
+    eyeProblemsDetails: { type: String },
+    ppeAllergy: { type: Boolean },
+    ppeAllergyDetails: { type: String },
+    rheumaticFever: { type: Boolean },
+    rheumaticFeverDetails: { type: String },
+    highBloodPressure: { type: Boolean },
+    highBloodPressureDetails: { type: String },
+    lowBloodPressure: { type: Boolean },
+    lowBloodPressureDetails: { type: String },
+    palpitations: { type: Boolean },
+    palpitationsDetails: { type: String },
+    heartAttack: { type: Boolean },
+    heartAttackDetails: { type: String },
+    angina: { type: Boolean },
+    anginaDetails: { type: String },
+    asthma: { type: Boolean },
+    asthmaDetails: { type: String },
+    chronicLungProblems: { type: Boolean },
+    chronicLungProblemsDetails: { type: String },
+    stroke: { type: Boolean },
+    strokeDetails: { type: String },
+    heartMurmur: { type: Boolean },
+    heartMurmurDetails: { type: String },
+    backProblems: { type: Boolean },
+    backProblemsDetails: { type: String },
+    jointProblems: { type: Boolean },
+    jointProblemsDetails: { type: String },
+    swollenLegs: { type: Boolean },
+    swollenLegsDetails: { type: String },
+    varicoseVeins: { type: Boolean },
+    varicoseVeinsDetails: { type: String },
+    rheumatism: { type: Boolean },
+    rheumatismDetails: { type: String },
+    migraine: { type: Boolean },
+    migraineDetails: { type: String },
+    drugReaction: { type: Boolean },
+    drugReactionDetails: { type: String },
+    visionCorrection: { type: Boolean },
+    visionCorrectionDetails: { type: String },
+    skinConditions: { type: Boolean },
+    skinConditionsDetails: { type: String },
+    alcoholHealthProblems: { type: Boolean },
+    alcoholHealthProblemsDetails: { type: String },
+    seriousIllnessDetails: { type: String },
+    recentIllHealth: { type: Boolean },
+    recentIllHealthComment: { type: String },
+    attendingClinic: { type: Boolean },
+    attendingClinicComment: { type: String },
+    hadChickenPox: { type: Boolean },
+    hadChickenPoxComment: { type: String },
+    otherCommunicableDisease: { type: Boolean },
+    otherCommunicableDiseaseComment: { type: String },
+
+    // Inoculations / Vaccinations
+    inocDiphtheria: { type: Boolean },
+    inocDiphtheriaComment: { type: String },
+    inocHepatitisB: { type: Boolean },
+    inocHepatitisBComment: { type: String },
+    inocTuberculosis: { type: Boolean },
+    inocTuberculosisComment: { type: String },
+    inocRubella: { type: Boolean },
+    inocRubellaComment: { type: String },
+    inocVaricella: { type: Boolean },
+    inocVaricellaComment: { type: String },
+    inocPolio: { type: Boolean },
+    inocPolioComment: { type: String },
+    inocTetanus: { type: Boolean },
+    inocTetanusComment: { type: String },
+    testedHIV: { type: Boolean },
+    testedHIVComment: { type: String },
+    inocOther: { type: String },
+    inocOtherComment: { type: String },
+    daysSickLastYear: { type: String },
+
+
+    houseNumberOrName: { type: String },
+postCode: { type: String },
+jobRole: { type: String },
+otherJobRole: { type: String },
+accountNumber: { type: String },
+sortCode: { type: String },
+bankName: { type: String },
+branchName: { type: String },
+buildingSocietyRollNo: { type: String },
+
+    // Consent & Declarations
     declarationCorrectUpload: { type: Boolean },
     declarationContactReferee: { type: Boolean },
-    criminalConviction: { type: Boolean },
-    criminalConvictionDetails: { type: String },
+    authorizeReferees: { type: Boolean }, // 👈 New field
+    disciplinaryInvestigation: { type: Boolean },
+    disciplinaryInvestigationDetails: { type: String },
+    abuseInvestigation: { type: Boolean },
+    abuseInvestigationDetails: { type: String },
+    roaDeclaration: { type: Boolean },
+    roaDeclarationDetails: { type: String },
     appliedBefore: { type: Boolean },
-    dataProcessingAccepted: { type: Boolean },
     termsAccepted: { type: Boolean },
+    dataProcessingAccepted: { type: Boolean },
+    consentMedicalDeclaration: { type: Boolean },
+    consentDataProtection: { type: Boolean },
+    consentVaccination: { type: Boolean },
+    consentTerminationClause: { type: Boolean },
+
+    // Timestamps
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
   },
   {
     timestamps: true,
   }
 );
+
 
 userSchema.statics.hashPassword = async function (
   plainTextPassword: string
